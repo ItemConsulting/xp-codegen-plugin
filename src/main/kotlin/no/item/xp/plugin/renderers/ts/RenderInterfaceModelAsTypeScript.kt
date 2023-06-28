7b490e249d385826cce7bc04b9c4ee30913d4a97
Tom Arild Jakobsen <tajakobsen@gmail.com>
2023-06-28 14:59:56 +0200

    Use type instead of interface
    The reason is that interfaces can be expanded, and are therefore
    less accurate, and can cause type issues that types doesn't cause.

--- src/main/kotlin/no/item/xp/plugin/renderers/ts/RenderInterfaceModelAsTypeScript.kt
+++ src/main/kotlin/no/item/xp/plugin/renderers/ts/RenderInterfaceModelAsTypeScript.kt
@@ -6,19 +6,8 @@ fun renderInterfaceModelAsTypeScript(model: InterfaceModel, typeName: String? = 
   val fieldList = model.fields.joinToString("\n\n") { renderInterfaceModelField(it, 1) }
 
   return """
-    #export interface ${getInterfaceName(model.nameWithoutExtension)} {
-    #$fieldList${renderBlankLines(model, typeName)}${renderTypeName(typeName)}
+    #export type ${getInterfaceName(model.nameWithoutExtension)} = {
+    #$fieldList
     #}
     #""".trimMargin("#")
 }
-
-fun renderTypeName(typeName: String?): String = typeName?.let {
-  """  /**
-  |   * GraphQL name. Also used for separating unions in TypeScript
-  |   */
-  |  __typename?: "$it";
-  """.trimMargin()
-} ?: ""
-
-fun renderBlankLines(model: InterfaceModel, typeName: String? = null): String =
-  if (model.fields.isNotEmpty() && typeName !== null) "\n\n" else ""
